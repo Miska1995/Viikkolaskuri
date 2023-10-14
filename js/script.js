504bd4ba4671540b5ee5dbf54ba0f8fefd1cff08
Miska1995 <Miskatevilin@gmail.com>
2023-10-14 19:01:18 +0300

    javascriptin korjaus

--- js/script.js
+++ js/script.js
@@ -18,32 +18,32 @@ function registerEventListeners() {
 
 function handleFormSubmit(e) {
     // ESTETÄÄN LOMAKKEEN OLETUSTOIMINTO (LÄHETYS)
-    e.preventDefault(); 
+    e.preventDefault();
 
     // HAETAAN KÄYTTÄJÄN SYÖTTÄMÄT ARVOT
-    const hours = parseFloat(document.getElementById('hours').value);  
+    const hours = parseFloat(document.getElementById('hours').value);
     const description = document.getElementById('description').value;
     const category = document.getElementById('category').value;
 
     // TEHDÄÄN TARKISTUKSET SYÖTETYILLE ARVOILLE
-    if (isNaN(hours) || hours <= 0 || description === '') {
-        alert("Tarkista tuntimäärä!");
+    if (isNaN(hours) || hours <= 0 || description === '' || category === '') {
+        alert("Tarkista tuntimäärä, kuvaus ja kategoria!");
         return;
     }
 
     // HAETAAN TIEDOT LOCALSTORAGESTA JA LISÄTÄÄN UUSI MERKINTÄ
     let entries = JSON.parse(localStorage.getItem('entries')) || [];
-    entries.push({hours, description, category});
+    entries.push({ hours, description, category });
     localStorage.setItem('entries', JSON.stringify(entries));
 
     // PÄIVITETÄÄN YHTEENVETO JA TYHJENNÄ LOMAKE
     updateSummary();
-    e.target.reset(); 
+    e.target.reset();
 }
 
 function handleResetButtonClick() {
     // KYSYTÄÄN VARMISTUS KÄYTTÄJÄLTÄ ENNEN TIETOJEN POISTAMISTA
-    if(confirm("Haluatko varmasti tyhjentää kaikki tiedot?")) {
+    if (confirm("Haluatko varmasti tyhjentää kaikki tiedot?")) {
         // POISTETAAN TIEDOT LOCALSTORAGESTA JA PÄIVITETÄÄN YHTEENVETO
         localStorage.removeItem('entries');
         updateSummary();
@@ -82,28 +82,28 @@ function updateSummary() {
     const summaryDiv = document.getElementById('summary');
     // TYHJENNETÄÄN "SUMMARY" ELEMENTIN SISÄLTÖ
     summaryDiv.innerHTML = "";
-    
+
     let summary = {};
 
-    // LUO JA TÄYDENTÄÄ YHTEENVETO-OBJEKTIA KAIKILLA TALLENNETUILLA ENTRIES-OBJEKTEILLA
+    // KÄY LÄPI JOKAINEN TALLENNETTU MERKINTÄ JA LASKE YHTEENVETO
     entries.forEach(entry => {
-        summary[entry.category] = (summary[entry.category] || {hours: 0, descriptions: []});
+        summary[entry.category] = summary[entry.category] || { hours: 0, descriptions: [] };
         summary[entry.category].hours += entry.hours;
         summary[entry.category].descriptions.push(entry.description);
     });
 
-    // NÄYTTÄÄ TAI PIILETTÄÄ YHTEENVEDON SEN MUKAAN, ONKO TALLETUKSIA
-    if(entries.length > 0) {
+    // NÄYTTÄÄ TAI PIILETTÄÄ YHTEENVETO SEN MUKAAN, ONKO TALLETUKSIA
+    if (entries.length > 0) {
         summaryDiv.style.display = "block";
-        
+
         // KÄY LÄPI JOKAINEN KATEGORIA JA LISÄÄ NE "SUMMARY" ELEMENTTIIN
         for (const category in summary) {
-            const pHours = document.createElement('p');
-            pHours.textContent = 'Kategoria: ${categoryMapping[entry.category]}';
-            ul.appendChild(categoryLi);
-            
+            const pCategory = document.createElement('p');
+            pCategory.textContent = `Kategoria: ${categoryMapping[category]}`;
+            summaryDiv.appendChild(pCategory);
+
             // LISÄTÄÄN KUVAUKSET, JOS NE OVAT OLEMASSA
-            if(summary[category].descriptions.length > 0) {
+            if (summary[category].descriptions.length > 0) {
                 const pDescriptions = document.createElement('p');
                 pDescriptions.textContent = `Kuvaus: ${summary[category].descriptions.join(', ')}`;
                 summaryDiv.appendChild(pDescriptions);
@@ -111,6 +111,6 @@ function updateSummary() {
         }
     } else {
         // PIILLOTTAA "SUMMARY" ELEMENTIN, JOS TALLETUKSIA EI OLE
-        summaryDiv.style.display = "none"; 
+        summaryDiv.style.display = "none";
     }
 }
